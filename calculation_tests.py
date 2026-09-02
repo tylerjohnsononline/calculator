d5f880f91d553f3b37509c10c9467dec685f99aa
Tyler <tylerjohnsononline@gmail.com>
2026-09-02 16:21:03 -0500

    add 1+2 tests

--- calculation_tests.py
+++ calculation_tests.py
@@ -9,7 +9,12 @@ class TestCalculations(unittest.TestCase):
     super().__init__(methodName)
   def test_one_plus_two(self):
       assert calculator.run_operation(1,"+",2) == 3
+  def test_one_plus_two_does_not_equal_four(self):
       assert calculator.run_operation(1,"+",2) != 4
+  def test_one_plus_two_is__integer(self):
+      assert isinstance(calculator.run_operation(1,"+",2), int)
+
+
 
 
 if __name__ == '__main__':
