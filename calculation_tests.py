00c897e352385c61af92b16eddd9ddc668bdbadf
Tyler <tylerjohnsononline@gmail.com>
2026-09-02 16:15:44 -0500

    moved calculation_tests.py file to main folder of project to allow easier testing

--- calculation_tests.py
+++ calculation_tests.py
@@ -2,14 +2,15 @@
 
 import unittest
 # from calculator import run_operation
-from calculator import calculator
+import calculator
 
 class TestCalculations(unittest.TestCase):
   def __init__(self, methodName = "runTest"):
     super().__init__(methodName)
-  def test_one_plus_two():
+  def test_one_plus_two(self):
       assert calculator.run_operation(1,"+",2) == 3
+      assert calculator.run_operation(1,"+",2) != 4
 
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main(verbosity=3)
